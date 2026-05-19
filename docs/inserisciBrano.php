906e25aa3223ab4e3f47d33e4b7efdc058528bc0
ciabby <emmanuelelonghi2@gmail.com>
2026-05-19 21:39:35 +0200

    Aggiornamenti
    aaa

--- docs/inserisciBrano.php
+++ docs/inserisciBrano.php
@@ -1,16 +1,22 @@
 <?php
 require "../config/db.php";
 
-$titolo = $_POST["titolo"];
+$titolo = $_POST["titolo_brano"];
 $artista = $_POST["Ascolti"];
 $durata = $_POST["durata_sec"];
 
 $sql = "INSERT INTO brani (titolo_brano, Ascolti, durata_sec) 
-        VALUES ('$titolo', '$artista', '$durata', '$annoUscita')";
+        VALUES ('$titolo', '$artista', '$durata')";
 
 $conn->query($sql);
 
 //Dopo aver inserito l'utente potrei fare diverse cose
+$sql = "SELECT id_artista,nome_arte FROM artisti ORDER BY nome_arte";
+$result = $conn->query($sql);
+$options = "";
+while ($row = $result->fetch_assoc()) {
+    $options .= "<option value='" . $row["id_artista"] . "'>" . $row["nome_arte"] . "</option>";
+}
 //scelgo di reindirizzare alla stessa pagina di inserimento html.
 header("Location: inserisciBrano.html");
 ?>
